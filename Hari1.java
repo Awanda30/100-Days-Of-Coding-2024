ad8c42752bb51913cafbe0ac9d25304666a9f922
Awanda30 <awandapolman@gmail.com>
2024-09-02 21:38:54 +0800

    Hari1.java

---
+++ Hari1.java
@@ -0,0 +1,26 @@
+package day1.coding;
+
+import java.util.Scanner;
+public class DAY1CODING {
+
+    public static void main(String[] args) {
+        String nama,kelas,alamat;
+        nama = "Awanda";
+        kelas = "informatika D 23";
+        alamat = "Tapango";
+        int umur = 20;
+        long tinggi = 150;
+        byte berat = 50;
+        boolean dayonecoding = true;
+        
+        System.out.println("nama  \t:"+nama);
+        System.out.println("kelas \t:"+kelas);
+        System.out.println("alamat\t:"+alamat);
+        System.out.println("umur  \t:"+umur);
+        System.out.println("tinggi \t:"+tinggi);
+        System.out.println("berat \t:"+berat);
+        System.out.println("dayonecoding \t:"+dayonecoding);
+             
+    }
+    
+}
