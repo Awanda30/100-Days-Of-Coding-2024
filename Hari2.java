c5fd1bdae53d37ddacd1018bdfacdba8fe15c4c7
Awanda30 <awandapolman@gmail.com>
2024-09-03 21:31:35 +0800

    Hari2.java

---
+++ Hari2.java
@@ -0,0 +1,10 @@
+package day2coding;
+public class DAY2CODING {
+    //public static void main(String[] args) {
+        //System.out.println("D INFORMATIKA");
+   // }
+    public static void main(String[] args){
+        System.out.println("INFORMATIKA D");
+    
+    }
+}
